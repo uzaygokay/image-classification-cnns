878259e8ccf5e474fbd5856f583fb87900367612
uzaygokay <uzaygokay@gmail.com>
2022-07-17 05:20:52 +0200

    added train related info

--- src/models/arguments.py
+++ src/models/arguments.py
@@ -8,14 +8,36 @@ def get_args():
     
     parser = argparse.ArgumentParser(description='Arguments for image classification')
 
+    #data module related
     parser.add_argument('-data_dir', type=str, help= 'Directory for downloaded dataset',
         default='./../../data/external'
     )
 
     parser.add_argument('-batch_size', type=int, help= 'Batch size',
-        default=20
+        default=64
     )
 
     parser.add_argument('-num_workers', type=int, help= 'Number of workers',
         default=2
+    )
+
+    parser.add_argument('-max_epochs', type=int, help= 'Number of workers',
+        default=3
+    )
+
+    #model related
+    parser.add_argument('-hidden_size', type=int, help= 'Hidden size of network',
+        default=64
+    )
+
+    parser.add_argument('-learning_rate', type=float, help= 'Learning rate',
+        default=2e-4
+    )
+
+    parser.add_argument('-weight_decay', type=float, help= 'Weight Decay',
+        default=1e-5
+    )
+
+    parser.add_argument('-dropout', type=float, help= 'Dropout rate',
+        default=0.1
     )
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,4 +1,51 @@
+from pytorch_ligthning.callbacks.progress.rich_progress import RichProgressBar
+from pythorch_lightning import Trainer
+
 from custom_logger import get_custom_logger
+from arguments import get_args
+from datamodule import CIFAR10
+from model import ConvModel
+
 
 if __name__ == '__main__':
-    logger = get_custom_logger(name)
+
+    args = get_args()
+    logger = get_custom_logger(__name__)
+
+    logger.info('Init Datamodule')
+    dm = CIFAR10(
+        data_dir=args.data_dir,
+        batch_size= args.batch_size,
+        num_workers=args.num_workers
+    )
+
+    logger.info('Init Model')
+    model = ConvModel(
+        *dm.data_dims,
+        num_classes=dm.num_classes,
+        hidden_size=args.hidden_size,
+        learning_rate=args.learning_rate,
+        weight_decay=args.weight_decay,
+        dropout=args.dropout
+    )
+
+    logger.info('Init Callbacks')
+    callbacks = []
+    callbacks.append(RichProgressBar(refresh_rate=200, leave=True))
+
+
+    logger.info('Init Trainer')
+    trainer = Trainer(
+        fast_dev_run = 3,
+        max_epochs = args.max_epochs,
+        enable_progress_bar=True,
+        callbacks=callbacks,
+        limit_train_batches =0.1,
+        limit_val_batches = 0.1
+    )
+
+    logger.info('Run Trainer')
+    trainer.fit(model,dm)
+
+    logger.info('Test the best model')
+    trainer.test(ckpt_path='best', datamodule=dm)
